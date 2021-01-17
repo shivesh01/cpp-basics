8ddde75296598a478126ecbd2f8bc73b8e801b85
Shivesh kumar singh <39302916+shivesh01@users.noreply.github.com>
2021-01-17 18:27:21 +0530

    Final changes

---
+++ Basics/Zero/Input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int num;
+    cout << "enter an integer: ";
+    cin >> num;
+    cout << "The number is " << num;
+    return 0;
+}
