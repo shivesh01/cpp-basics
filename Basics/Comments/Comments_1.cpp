dcbf2d65b0f5d44ee37fa2fc5081a598a73f60b8
Shivesh <singhshiveshkumar@gmail.com>
2021-01-21 15:56:33 +0530

    Add files via upload

---
+++ Basics/Comments/Comments_1.cpp
@@ -0,0 +1,17 @@
+// single comment
+
+int a; // declaring a variable
+
+//Multi-line comments
+
+/* declaring a variable 
+to store salary to employess
+*/
+int salary = 2000;
+
+// we can use comments as a debugging tool
+
+//  shortcut cmd + /
+
+// If we write comments on our code, It will be easier for us to understand the code in the future.
+// Also, it will be easier for your fellow developers to understand the code.
