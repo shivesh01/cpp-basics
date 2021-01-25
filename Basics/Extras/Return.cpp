e5572e459afb0f2ebf1cc78158f58cf119a5b70b
Shivesh <singhshiveshkumar@gmail.com>
2021-01-25 08:37:20 +0530

    Create Return.cpp

---
+++ Basics/Extras/Return.cpp
@@ -0,0 +1,19 @@
+// Return by reference
+
+#include <iostream>
+using namespace std;
+
+int num;
+
+int& test();
+
+int main()
+{
+    test() = 5;
+    cout << num;
+    return 0;
+}
+int& test()
+{
+    return num;
+}
