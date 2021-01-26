0855ea097e1f2c51d9d43819105c18ff21d3d2fd
Shivesh <singhshiveshkumar@gmail.com>
2021-01-26 07:35:12 +0530

    added multi-dimensional arrays

---
+++ Basics/Multi-dimensional Arrays/M_array_2.cpp
@@ -0,0 +1,30 @@
+// 2-D ARRAY
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numbers[2][3];
+
+    cout << "Enter 6 numbers:" << endl;
+
+    for (int i = 0; i < 2; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            cin >> numbers[i][j];
+        }
+    }
+
+    cout << "The numbers are: " << endl;
+
+    for (int i = 0; i < 2; ++i)
+    {
+        for (int j = 0; j < 3; ++j)
+        {
+            cout << "numbers[" << i << "][" << j << "]: " << numbers[i][j] << endl;
+        }
+    }
+
+    return 0;
+}
