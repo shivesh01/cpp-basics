6cbf639209d10acc5dd2efec9fafbce6fd608f10
shivesh01 <singhshiveshkumar@gmail.com>
2021-02-11 18:40:35 +0530

    added 21 to 25

---
+++ Basics/Examples/23.cpp
@@ -0,0 +1,19 @@
+// Multiply two Numbers
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    double firstNumber, secondNumber, productofTwoNumbers;
+    cout<<"Enter two numbers: ";
+
+    cin>>firstNumber>>secondNumber;
+
+    productofTwoNumbers = firstNumber*secondNumber;
+
+    cout<<"Product = "<<productofTwoNumbers;
+
+    return 0;
+
+}
