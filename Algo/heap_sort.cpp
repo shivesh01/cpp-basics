2d3ee0b69d711a2afc79b03dd82534085013e55c
shivesh01 <singhshiveshkumar@gmail.com>
2021-04-11 23:20:14 +0530

    added heap sort

---
+++ Algo/heap_sort.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+// Heapify function
+void heapify(int arr[], int n, int i)
+{
+    // Find largest among root, left child and right child
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    // swap and continue heapifying if root is not largest
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+// main function to do heap sort
+void heapsort(int arr[], int n)
+{
+    // Build max heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+    // Heap sort
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(arr[0], arr[i]);
+
+        // Heapify root element to get highest element at root again
+        heapify(arr, i, 0);
+    }
+}
+
+// print an array
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+// Driver code
+int main()
+{
+    int arr[] = {1, 12, 7, 5, 4, 19};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    heapsort(arr, n);
+
+    cout << "Sorted array is \n";
+    printArray(arr, n);
+}
