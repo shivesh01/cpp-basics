9369e75fd7235fbac361f044f262d2fe9e36ded9
shivesh01 <singhshiveshkumar@gmail.com>
2021-04-25 22:15:04 +0530

    added huffman

---
+++ Algo/huffman.cpp
@@ -0,0 +1,12 @@
+// Huffman Coding Algorithm in C++
+
+/* pseudocode
+create a priority queue Q consisting of each unique character.
+sort then in ascending order of their frequencies.
+for all the unique characters:
+    create a newNode
+    extract minimum value from Q and assign it to leftChild of newNode
+    extract minimum value from Q and assign it to rightChild of newNode
+    calculate the sum of these two minimum values and assign it to the value of newNode
+    insert this newNode into the tree
+return rootNode*/
