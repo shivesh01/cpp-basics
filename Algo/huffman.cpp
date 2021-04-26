c3fbff675696bc96aa5123f77fc9f68fbdb834de
shivesh01 <singhshiveshkumar@gmail.com>
2021-04-26 22:14:07 +0530

    added huffman

--- Algo/huffman.cpp
+++ Algo/huffman.cpp
@@ -10,3 +10,134 @@ for all the unique characters:
     calculate the sum of these two minimum values and assign it to the value of newNode
     insert this newNode into the tree
 return rootNode*/
+
+// Huffman Coding in C++
+
+#include <iostream>
+using namespace std;
+
+#define MAX_TREE_HT 50
+
+struct MinHNode
+{
+    unsigned freq;
+    char item;
+    struct MinHNode *left, *right;
+};
+
+struct MinH
+{
+    unsigned size;
+    unsigned capacity;
+    struct MinHNode **array;
+};
+
+// Creating Huffman tree node
+struct MinHNode *newNode(char item, unsigned freq)
+{
+    struct MinHNode *temp = (struct MinHNode *)malloc(sizeof(struct MinHNode));
+
+    temp->left = temp->right = NULL;
+    temp->item = item;
+    temp->freq = freq;
+
+    return temp;
+}
+
+// Create min heap using given capacity
+struct MinH *createMinH(unsigned capacity)
+{
+    struct MinH *minHeap = (struct MinH *)malloc(sizeof(struct MinH));
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+    minHeap->array = (struct MinHNode **)malloc(minHeap->capacity * sizeof(struct MinHNode *));
+    return minHeap;
+}
+
+// Print the array
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; ++i)
+        cout << arr[i];
+
+    cout << "\n";
+}
+
+// Swap function
+void swapMinHNode(struct MinHNode **a, struct MinHNode **b)
+{
+    struct MinHNode *t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// Heapify
+void minHeapify(struct MinH *minHeap, int idx)
+{
+    int smallest = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+
+    if (left < minHeap->size && minHeap->array[left]->freq < minHeap->array[smallest]->freq)
+        smallest = left;
+
+    if (right < minHeap->size && minHeap->array[right]->freq < minHeap->array[smallest]->freq)
+        smallest = right;
+
+    if (smallest != idx)
+    {
+        swapMinHNode(&minHeap->array[smallest],
+                     &minHeap->array[idx]);
+        minHeapify(minHeap, smallest);
+    }
+}
+
+// Check if size if 1
+int checkSizeOne(struct MinH *minHeap)
+{
+    return (minHeap->size == 1);
+}
+
+// Extract the min
+struct MinHNode *extractMin(struct MinH *minHeap)
+{
+    struct MinHNode *temp = minHeap->array[0];
+    minHeap->array[0] = minHeap->array[minHeap->size - 1];
+
+    --minHeap->size;
+    minHeapify(minHeap, 0);
+
+    return temp;
+}
+
+// Insertion
+void insertMinHeap(struct MinH *minHeap, struct MinHNode *minHeapNode)
+{
+    ++minHeap->size;
+    int i = minHeap->size - 1;
+
+    while (i && minHeapNode->freq < minHeap->array[(i - 1) / 2]->freq)
+    {
+        minHeap->array[i] = minHeap->array[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+
+    minHeap->array[i] = minHeapNode;
+}
+
+// BUild min heap
+void buildMinHeap(struct MinH *minHeap)
+{
+    int n = minHeap->size - 1;
+    int i;
+
+    for (i = (n - 1) / 2; i >= 0; --i)
+        minHeapify(minHeap, i);
+}
+
+int isLeaf(struct MinHNode *root)
+{
+    return !(root->left) && !(root->right);
+}
+
