45e61e1ce82cf01bdb45cf64f4d84cabc081a016
shivesh01 <singhshiveshkumar@gmail.com>
2021-04-29 10:53:02 +0530

    added warshall algo

---
+++ Algo/warshall_algo.cpp
@@ -0,0 +1,11 @@
+// Floyd-Warshall Algorithm in C++
+
+/* pseudocode
+n = no of vertices
+A = matrix of dimension n*n
+for k = 1 to n
+    for i = 1 to n
+        for j = 1 to n
+            Ak[i, j] = min (Ak-1[i, j], Ak-1[i, k] + Ak-1[k, j])
+return A
+*/
